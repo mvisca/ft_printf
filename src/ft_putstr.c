0d47433574b6dead15472ccffbd2f87d6cff6c91
mvisca-g <mvisca-g@car15s6.42barcelona.com>
2023-05-29 20:37:37 +0200

    ok update refactoring

--- src/ft_putstr.c
+++ src/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: mvisca-g <mvisca-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 16:41:16 by mvisca-g          #+#    #+#             */
-/*   Updated: 2023/05/29 19:33:28 by mvisca-g         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:37:21 by mvisca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int	ft_putstr(va_list params, int *i)
 		control = write (1, value, ft_strlen(value));
 	else
 		control = write(1, "(null)", 6);
-	*i = *i + 1;
+	(*i)++;
 	return (control);
 }
