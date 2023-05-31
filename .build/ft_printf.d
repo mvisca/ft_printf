011f0fd5e8490d9060a9ab2e6f71ddff42fe905c
mvisca <martin.visca@gmail.com>
2023-05-31 19:07:10 +0200

    clean

--- .build/ft_printf.d
+++
@@ -1,4 +0,0 @@
-.build/ft_printf.o: src/ft_printf.c src/../include/ft_printf.h \
- ft_libft/include/libft.h
-src/../include/ft_printf.h:
-ft_libft/include/libft.h:
--- .build/ft_putbasedec.d
+++
@@ -1,4 +0,0 @@
-.build/ft_putbasedec.o: src/ft_putbasedec.c src/../include/ft_printf.h \
- ft_libft/include/libft.h
-src/../include/ft_printf.h:
-ft_libft/include/libft.h:
